cb8d5a5e4a194cbf95369aca1a2362bde783bc00
mlemoine28 <145514725+mlemoine28@users.noreply.github.com>
2024-02-08 21:12:01 -0330

    added display none and block to play now button

--- Beat-Sync-Challenge/index.js
+++ Beat-Sync-Challenge/index.js
@@ -1,6 +1,7 @@
 let playnow = document.querySelector('#playnow')
-
 function onClick() {
+    document.querySelector('#intropage').style.display = "none";
+    document.querySelector('#wholegame').style.display = "block";
     console.log('hello')    
 }
 
