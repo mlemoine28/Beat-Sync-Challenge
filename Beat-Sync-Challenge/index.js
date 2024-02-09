6124600f8253f935dafc65b94152a055f8fddfab
mlemoine28 <145514725+mlemoine28@users.noreply.github.com>
2024-02-09 13:55:55 -0330

    added leaderboard page with JS

--- Beat-Sync-Challenge/index.js
+++ Beat-Sync-Challenge/index.js
@@ -1,8 +1,20 @@
 let playnow = document.querySelector('#playnow')
+
+playnow.addEventListener('click', onClick)
+
 function onClick() {
     document.querySelector('#intropage').style.display = "none";
     document.querySelector('#wholegame').style.display = "block";
     console.log('hello')    
 }
 
-playnow.addEventListener('click', onClick)
+
+
+let leaderboards = document.querySelector('#leaderboards')
+
+leaderboards.addEventListener('click',onClick2)
+
+function onClick2() {
+    document.querySelector('#wholegame').style.display = "none";
+    document.querySelector('#highscores').style.display = "block";
+}
