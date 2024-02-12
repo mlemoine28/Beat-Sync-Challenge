0891c8361bc5cb593506caf85d8efacb93c0928b
mlemoine28 <145514725+mlemoine28@users.noreply.github.com>
2024-02-12 13:07:48 -0330

    score rankings how to play

--- Beat-Sync-Challenge/index.js
+++ Beat-Sync-Challenge/index.js
@@ -18,3 +18,12 @@ function onClick2() {
     document.querySelector('#wholegame').style.display = "none";
     document.querySelector('#highscores').style.display = "block";
 }
+
+let howtoplay = document.querySelector('#howtoplay')
+
+howtoplay.addEventListener('click', onClick3)
+
+function onClick3() {
+    document.querySelector('#intropage').style.display = "none";
+    document.querySelector('#howtoplaypage').style.display = "block";
+}
