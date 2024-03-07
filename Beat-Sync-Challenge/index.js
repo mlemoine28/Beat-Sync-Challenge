b705ce6d650d1a210e504646bce1ee1fbce6a9c0
mlemoine28 <145514725+mlemoine28@users.noreply.github.com>
2024-03-07 11:20:51 -0330

    cleaned up code; also added "for" loop for array of beats

--- Beat-Sync-Challenge/index.js
+++ Beat-Sync-Challenge/index.js
@@ -1,6 +1,15 @@
 let playnow = document.querySelector('#playnowbutton');
 let song = document.querySelector('#song');
 let leaderboards = document.querySelector('#leaderboards')
+let kickButton = document.querySelector('#kickButton')
+let howtoplay = document.querySelector('#howtoplaybutton')
+let back = document.querySelector('#back')
+let gotIt = document.querySelector('#got-it-button')
+let modal = document.querySelector('.howtoplaymodal')
+let howtoplay2 = document.querySelector('#howtoplay2')
+let kickDrumSound = document.querySelector('#kickdrumsound')
+let songButton = document.querySelector('#songButton')
+let smileyfacemain = document.querySelector('#smileyface');
 
 playnow.addEventListener('click', onPlayNowClick);
 
@@ -9,18 +18,12 @@ function onPlayNowClick() {
     document.querySelector('#wholegame').style.display = 'block';  
 }
 
-
-
-
-
 leaderboards.addEventListener('click',onLeaderboardsClick)
 
 function onLeaderboardsClick() {
     document.querySelector('#wholegame').style.display = "none";
     document.querySelector('#highscores').style.display = "block";
 }
-
-let howtoplay = document.querySelector('#howtoplaybutton')
 
 howtoplay.addEventListener('click', onHowToPlayClick)
 
@@ -31,8 +34,6 @@ function onHowToPlayClick() {
     
 }
 
-let back = document.querySelector('#back')
-
 back.addEventListener('click', onBackClick)
 
 function onBackClick() {
@@ -40,19 +41,14 @@ function onBackClick() {
     document.querySelector('#wholegame').style.display="block";
 }
 
-let gotIt = document.querySelector('#got-it-button')
-
 gotIt.addEventListener('click', () => {
-    let modal = document.querySelector('.howtoplaymodal')
+    
     modal.close();
     modal.style.display = "none";
 })
 
-let howtoplay2 = document.querySelector('#howtoplay2')
-
 howtoplay2.addEventListener('click', onHowToPlayClick)
 
-let smileyfacemain = document.querySelector('#smileyface');
 smileyfacemain.addEventListener('click', (e) => {
     e.target.classList.add('smileyimage');
     let animations = e.target.getAnimations();
@@ -60,13 +56,13 @@ smileyfacemain.addEventListener('click', (e) => {
         animation.currentTime = 0;
     }
 });
+
 smileyfacemain.addEventListener('animationend', (e) => {
     e.target.classList.remove('smileyimage');
 });
 
-let kickButton = document.querySelector('#kickButton')
 kickButton.addEventListener('click', () => {
-    let kickDrumSound = document.querySelector('#kickdrumsound')
+    
     if (kickDrumSound.paused) {
         kickDrumSound.play();
     } else {
@@ -75,7 +71,6 @@ kickButton.addEventListener('click', () => {
     kickDrumSound.loop = true;
 })
 
-let songButton = document.querySelector('#songButton')
 songButton.addEventListener('click', () => {
     if (song.paused) {
         song.play();
@@ -84,26 +79,19 @@ songButton.addEventListener('click', () => {
     }
 })
 
-
-
-
 let lastTimestamp = 0;
 
 function animate(timestamp) {
     if (!lastTimestamp) { //This is for the purpose of making sure that lastTimestamp only starts after rendering the second frame. Needed for accurately calculating deltaTime.
         lastTimestamp = timestamp;
     } else {
-
     const deltaTime = timestamp - lastTimestamp; //the CURRENT time minus the time since the last frame. Basically, the amount of times between frames.
-
     lastTimestamp = timestamp; //this updates lastTimestamp for the next frame, otherwise the times won't be consistent
     console.log(deltaTime);
-    
 }
     requestAnimationFrame(animate);
 }
-requestAnimationFrame(animate);
-
+    requestAnimationFrame(animate);
 
 console.log(lastTimestamp);
 
@@ -113,20 +101,17 @@ document.addEventListener('DOMContentLoaded', () => {
     let lastBeatTime = 0;
 })
 
-function checkBeat(timestamp) {
+/*function checkBeat(timestamp) {
     const currentTime = kickDrumSound.currentTime * 1000;
     if (currentTime - lastBeatTime >= beatInterval) {
       lastBeatTime += beatInterval;
     }
-
     requestAnimationFrame(checkBeat);
   }
   requestAnimationFrame(checkBeat);
 
   console.log(checkBeat);
  
-
-
    document.addEventListener('DOMContentLoaded', () => {
     const audio = document.getElementById('song');
     const feedbackElement = document.getElementById('feedback');
@@ -134,5 +119,6 @@ function checkBeat(timestamp) {
 
     const keyBeat = [476, 952, 1428, 1904, 2380, 2856, 3332, 3808, 4284, 4760, 5236, 5712, 6188, 6664, 7140, 7616, 8092, 8568, 9044, 9520, 9996, 10472, 10948, 11424, 11900, 12376, 12852, 13328, 13804, 14280, 14756, 15232, 15708, 16184, 16660]
   })
-
-  
+*/
+  for (let keyBeat = 476; keyBeat <= 40000; keyBeat += 476)
+  console.log(keyBeat);
